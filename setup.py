aeb25cf9597825039079c991d02701f3a483361f
Alexander Shorin <kxepal@gmail.com>
2016-07-21 16:57:45 +0300

    I think, we're good for beta now

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     test_suite='tests',
 
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
